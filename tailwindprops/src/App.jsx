3ec80fa844f632434a8a6ea5b48265f3084dd50d
Rohit Verma <rohit.verma272727@gmail.com>
2023-10-15 22:35:54 +0530

    set up tailwind

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-yellow-500'>Hello tailwind</h1>
+      <h2 className='font-bold underline'>Rohit</h2>
+    </>
+  )
+}
+
+export default App
