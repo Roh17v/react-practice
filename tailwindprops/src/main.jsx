cc93e8f71efae5bd068f0a0a170f8fc46beedabf
Rohit Verma <rohit.verma272727@gmail.com>
2023-10-22 10:26:23 +0530

    props in react

--- tailwindprops/src/main.jsx
+++ tailwindprops/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Card from './components/card.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <Card username="Rohit" />
+      <Card username="hitesh" />
+      <Card username="Somesh" />
+  </React.StrictMode>
 )
